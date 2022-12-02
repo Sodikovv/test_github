e54935f907bc1aaa3c8138d72a7b19c8c6694d77
KhanhNguyen <nguyenhoangnamkhanh@student.utwente.nl>
2022-12-02 14:17:56 +0100

    Khanh's edit 3

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hiiiii");
         System.out.print("Edited");
-        System.out.println("What's up? tesing something");
+        System.out.println("What's up? tesing something, test something again");
     }
 }
