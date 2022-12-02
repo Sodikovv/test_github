24bdaba872e32b2ec8fdd412c8e9f4babe7b176c
Sodikov, A.N. (Ammar, Student B-TCS) <a.n.sodikov@student.utwente.nl>
2022-12-02 14:19:02 +0100

    new sout

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hiiiii");
         System.out.print("Edited");
-        System.out.println("What's up?");
+        System.out.println("What's up? aaaaaaaaaaaaaaaaaaaa");
     }
 }
